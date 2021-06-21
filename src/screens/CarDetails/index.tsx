cd59048bfb0e8717a014f18b7d3ba9c6983b649e
joaobenthin <joaopaulobenthin@hotmail.com>
2021-06-21 19:56:09 -0300

    Componente de Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
+
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
   Container,
-  Header
+  Header,
+  CarImages
 } from './styles';
 
 export function CarDetails(): JSX.Element {
   return (
     <Container>
       <Header>
-        <BackButton />
+        <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={['https://img2.gratispng.com/20180628/stg/kisspng-2018-audi-s5-3-0t-premium-plus-coupe-audi-rs5-2017-2018-audi-a5-coupe-5b35130451d959.0738564215302049323353.jpg']}
+        />
+      </CarImages>
     </Container>
   );
 }
