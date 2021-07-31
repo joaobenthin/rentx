8d3a3a98b98cca68319d88ca876331ca432866b6
joaobenthin <joaopaulobenthin@hotmail.com>
2021-07-31 13:29:17 -0300

    Componente de Acessórios

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
 
 import {
   Container,
@@ -16,6 +24,7 @@ import {
   Period,
   Price,
   About,
+  Accessories,
 } from './styles';
 
 export function CarDetails(): JSX.Element {
@@ -43,6 +52,15 @@ export function CarDetails(): JSX.Element {
             <Price>R$ 580</Price>
           </Rent>
         </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={speedSvg} />
+          <Accessory name="3.2s" icon={accelerationSvg} />
+          <Accessory name="800 HP" icon={forceSvg} />
+          <Accessory name="Gasolina" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={exchangeSvg} />
+          <Accessory name="2 pessoas" icon={peopleSvg} />
+        </Accessories>
 
         <About>
           Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
